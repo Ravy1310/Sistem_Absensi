bdfd54f0c3929c221dea699516acf51b5df05e2a
Rafi Isnanto Syahlefi <rafiisnanto2005@gmail.com>
2025-03-13 10:10:07 +0700

    Manajemen_Karyawan

---
+++ Absensi/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/Login/login.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Modern Login UI");
+        primaryStage.setScene(scene);
+ // Maksimalkan jendela saat startup
+    primaryStage.setMaximized(true);
+
+    // Sesuaikan ukuran dengan layar utama
+    Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+    primaryStage.setWidth(screenBounds.getWidth());
+    primaryStage.setHeight(screenBounds.getHeight());
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
